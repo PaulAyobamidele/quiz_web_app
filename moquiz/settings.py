17603bee28c2af9085ca5bfc069596c6f2e5790d
Ayobamidele <paulikeystreble@gmail.com>
2023-03-29 05:24:06 +0100

    Added env

--- moquiz/settings.py
+++ moquiz/settings.py
@@ -20,12 +20,20 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-&3s--_8exo9uwj1jws7mv+_cv1yvnamirke*fl&-4i4^n9^5oq"
+# import environ
+# env = environ.Env()
+# environ.Env.read_env()
 
+SECRET_KEY = 'django-insecure-&3s--_8exo9uwj1jws7mv+_cv1yvnamirke*fl&-4i4^n9^5oq'
+# SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
+
+
 ALLOWED_HOSTS = []
+
+
 
 
 # Application definition
